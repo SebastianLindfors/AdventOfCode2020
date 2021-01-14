e74eeb072599bb4eec42fd7f4faad73b9b006f81
SebastianLindfors <sebastianlindfors@gmail.com>
2021-01-14 16:51:44 +0100

    Refactored and cleaned up Day 19 Part 1 solution

--- src/main/java/com/AOC2020/Tasks/Day19.java
+++ src/main/java/com/AOC2020/Tasks/Day19.java
@@ -28,7 +28,6 @@ public @Data class Day19 extends AoCChallenge{
     for (int i = 0; i < inputData.length; i++) {
       if (inputData[i].equals("")) {
         breakPoint = i;
-        System.out.println("Breakpoint:" + breakPoint);
         break;
       }
     }
@@ -41,14 +40,12 @@ public @Data class Day19 extends AoCChallenge{
       new MessageRule(ruleLine, messageRules);
     }
 
-    System.out.println("Rules read in: " + messageRules.size());
 
     while(messageRules.get("0").getValidStringsNonRecursive() == null) {
 
       for ( MessageRule messageRule : messageRules.values())
       {
         if (messageRule.getValidStringsNonRecursive() == null) {
-          System.out.println("Attempting to create strings for rule " + messageRule.ruleId);
           messageRule.computeValidStringsArrayNonRecursive();
         }
       }
@@ -111,13 +108,6 @@ public @Data class Day19 extends AoCChallenge{
 
     }
 
-    String[] getValidStringsRecursive() {
-      if (validStrings.length == 0) {
-        computeValidStringsArrayRecursive();
-      }
-      return validStrings;
-    }
-
     String[] getValidStringsNonRecursive() {
       if (validStrings.length == 0) {
         return null;
@@ -136,8 +126,6 @@ public @Data class Day19 extends AoCChallenge{
 
       for (int i = 0; i < subRuleStrings.length; i++) { // 1 3 | 3 1
 
-        System.out.println("TESTING: " + subRuleStrings[i]);
-
         int neededLength = (int) subRuleStrings[i].chars().filter(ch -> ch == ' ').count();
         String[][] subStrings = new String[neededLength + 1][];
         int subRuleStringIndex = 0;
@@ -148,14 +136,12 @@ public @Data class Day19 extends AoCChallenge{
           if (nexSpaceIndex != -1) {
             subStrings[substringCounter] = rulesMap.get(subRuleStrings[i].substring(subRuleStringIndex, nexSpaceIndex)).getValidStringsNonRecursive();
             if (subStrings[substringCounter] == null) {
-              System.out.println("Rule " + subRuleStrings[i].substring(subRuleStringIndex, nexSpaceIndex) + " not established, continuing...");
               return;
             }
           }
           else {
             subStrings[substringCounter] = rulesMap.get(subRuleStrings[i].substring(subRuleStringIndex)).getValidStringsNonRecursive();
             if (subStrings[substringCounter] == null) {
-              System.out.println("Rule " + subRuleStrings[i].substring(subRuleStringIndex) + " not established, continuing...");
               return;
             }
           }
@@ -207,71 +193,6 @@ public @Data class Day19 extends AoCChallenge{
 
       validStrings = tempValidStrings;
     }
-
-
-    private void computeValidStringsArrayRecursive() {
-
-      if (subRuleStrings[0].charAt(0) == '"') {
-        System.out.println("Returning: " + subRuleStrings[0].substring(1,2));
-        validStrings = new String[] { subRuleStrings[0].substring(1,2)};
-        return;
-      }
-
-      for (int i = 0; i < subRuleStrings.length; i++) { // 13 | 31
-
-        String[][] subStrings = new String[subRuleStrings[i].length()][];
-        for (int j = 0; j < subRuleStrings[i].length(); j++) { // 13
-          //System.out.println("Key: " + subRuleStrings[i].substring(j,j+1));
-          subStrings[j] = rulesMap.get(subRuleStrings[i].substring(j,j+1)).getValidStringsRecursive();
-          if (subStrings[j] == null) {
-            System.out.println("NULLPOINTER EXCEPTION INCOMING, BITCHES");
-          }
-        }
-
-        int product = 1;
-        for (int j = 0; j < subStrings.length; j++) {
-          product *= subStrings[j].length;
-        }
-
-        int[] counter = new int[subStrings.length];
-        String[] outputStrings = new String[product];
-        int outputPointer = 0;
-
-        boolean finished = false;
-        while(!finished) {
-
-          String outputString = "";
-          for (int j = 0; j < counter.length; j++) {
-           outputString += subStrings[j][counter[j]];
-          }
-          outputStrings[outputPointer++] = outputString;
-          System.out.println(outputString);
-
-          int counterPointer = counter.length - 1;
-          while(counterPointer >= 0) {
-            System.out.println("Pointer: " + counterPointer +", Counter: " + counter[counterPointer]);
-            counter[counterPointer]++;
-            if (counter[counterPointer] == subStrings[counterPointer].length) {
-              if (counterPointer == 0) {
-                finished = true;
-              }
-              counter[counterPointer] = 0;
-              counterPointer--;
-            }
-            else {
-              break;
-            }
-          }
-        }
-        validStrings = ArrayUtils.addAll(validStrings, outputStrings);
-
-        System.out.println("-----");
-      }
-
-
-    }
-
-
   }
 
 }
